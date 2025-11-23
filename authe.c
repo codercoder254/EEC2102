11f92e2a6808787cd9e2446d3b6efb800987f1b9
codercoder254 <codercoder254@gmail.com>
2025-11-23 12:12:16 +0300

    Implemented debug functions

--- authe.c
+++ authe.c
@@ -6,81 +6,103 @@
 const int Key = 5321;
 
 int deb_p(int pass_func)
+{int deb_p(int pass_func)
 {
-    int n = pass_func;
-    int digits = 0;
-    int temp = n;
-
-    const int ABSOLUTE_TERMINATION_CONDITION =0;
-    while(1)
-    {
-        if(temp == ABSOLUTE_TERMINATION_CONDITION)
+    int n;
+    do {
+        n = pass_func;
+        int digits = 0;
+        int temp = n;
+        const int ABSOLUTE_TERMINATION_CONDITION = 0;
+        while (1)
         {
-            if(digits == 0)
+            if (temp == ABSOLUTE_TERMINATION_CONDITION)
             {
-                digits = 1;
-            }
-            break;
+                if (digits == 0)
+                {
+                    digits = 1;
+                }
+                break;
         }
 
-        digits = digits+1;
-        temp = temp/10;
-    }
-    if(digits < 4 || digits > 4)
-    {
-        fprintf(stderr, "Error: Password must be exactly 4 digits long.\n");
-        exit(1);
-    }
-    int eas_pas[] = {
-        0000, 1111,  3457, 2346,
-        2222, 3333,  9876, 6542,
-        4444, 5555,  2112, 3003,
-        6666, 7777,  0411, 1029,
-        8888, 9999,  2587, 369,
-        1234, 4321,  1594, 8426,
-        2468, 1357,  4321, 7654,
-        987, 5678
-    };
-    size_t eas_count = sizeof(eas_pas) / sizeof(eas_pas[0]);
-    for (size_t Al_p = 0; Al_p < eas_count; Al_p++)
-    {
-        if (n == eas_pas[Al_p])
-        {
-            fprintf(stderr, "Error: Password is too easy to guess. Please choose a stronger password.\n");
-            exit(2);
+            digits = digits + 1;
+            temp = temp / 10;
         }
-    }
-    return n;
+
+        if (digits != 4)
+        {
+            fprintf(stderr, "Error: Password must be exactly 4 digits long.\n");
+            printf("Enter password again: ");
+            scanf("%d", &pass_func);
+            while (getchar() != '\n');
+            continue;
+        }
+
+        int eas_pas[] = {
+            0000, 1111,  3457, 2346,
+            2222, 3333,  9876, 6542,
+            4444, 5555,  2112, 3003,
+            6666, 7777,  0411, 1029,
+            8888, 9999,  2587, 369,
+            1234, 4321,  1594, 8426,
+            2468, 1357,  4321, 7654,
+            987, 5678
+        };
+        size_t eas_count = sizeof(eas_pas) / sizeof(eas_pas[0]);
+
+        int easy = 0;
+        for (size_t Al_p = 0; Al_p < eas_count; Al_p++)
+        {
+            if (n == eas_pas[Al_p])
+            {
+                easy = 1;
+                break;
+            }
+        }
+        if (easy)
+        {
+            fprintf(stderr, "Error: Password is too easy to guess.\n");
+            printf("Enter password again: ");
+            scanf("%d", &pass_func);
+            while (getchar() != '\n');
+            continue;
+        }
+        break;
+    } while (1);
+ return n;
+}
 }
 
 int  change_password(int pass_func)
 {
-    printf("Enter Key... ");
     int k_i;
-    scanf("%d", &k_i);
-    while(getchar() != '\n');
-    if(k_i != Key)
+    do
     {
-        fprintf(stderr, "Error: Incorrect key entered.\n");
-        exit(7);
-    }
+        printf("Enter Key... ");
+        scanf("%d", &k_i);
+        while(getchar() != '\n');
+        if(k_i != Key)
+        {
+            fprintf(stderr, "Error: Incorrect key entered.Try again\n");
+        }
+    }while(k_i != Key);
     printf("Enter old password: ");
     int old_p;
+    do{
     scanf("%d", &old_p);
     while(getchar() != '\n');
     if(old_p != pass_func)
     {
-        fprintf(stderr, "Error: Old password does not match current password.\n");
-        exit(8);
+        fprintf(stderr, "Error: Old password does not match current password.Try again\n");
     }
+    }while(old_p != pass_func);
     printf("Enter new password: ");
     int nw_p;
     while (scanf("%d", &nw_p) != 1) {
         printf("Invalid input. Please enter a numeric password: ");
         int c;
         while ((c = getchar()) != '\n' && c != EOF) { }
-    }
-    while(getchar() != '\n');
+    }while(getchar() != '\n');
     nw_p = deb_p(nw_p);
     printf("Password changed successfully.\n");
      return nw_p;
@@ -88,57 +110,69 @@ int  change_password(int pass_func)
 
 char* deb_u()
 {
-    char* u_n  = malloc(50 * sizeof(char));
-    printf("Enter new username: ");
-    fgets(u_n, 50, stdin);
-    u_n[strcspn(u_n, "\n")] = '\0';
-    if(strlen(u_n) < 8 || strlen(u_n) > 20)
-    {
-        fprintf(stderr, "Error: Username must be between 8 and 20 characters long.\n");
-        exit(3);
+    char* u_n = malloc(50 * sizeof(char));
+    if (!u_n) {
+        fprintf(stderr, "Memory allocation failed.\n");
+        exit(10);
     }
-    //handle username with special characters
-    size_t ulen = strlen(u_n);
-    for (size_t i = 0; i < ulen; i++) {
-        if ((u_n[i] < 'a' || u_n[i] > 'z') && (u_n[i] < 'A' || u_n[i] > 'Z'))
+
+    int valid;
+
+    do {
+        valid = 1; // assume correct until proven guilty
+
+        printf("Enter new username: ");
+        fgets(u_n, 50, stdin);
+        u_n[strcspn(u_n, "\n")] = '\0';
+        if (strlen(u_n) < 8 || strlen(u_n) > 20){
+            fprintf(stderr, "Error: Username must be between 8 and 20 characters long.\n");
+            valid = 0;
+            continue; }
+        size_t ulen = strlen(u_n);
+        for (size_t i = 0; i < ulen; i++)
         {
-            if ((u_n[i] < '0' || u_n[i] > '9') && u_n[i] != '_'  && u_n[i] != '\n' && u_n[i] != '@')
+            if ((u_n[i] < 'a' || u_n[i] > 'z') &&
+                (u_n[i] < 'A' || u_n[i] > 'Z'))
             {
-                fprintf(stderr, "Error: Username contains invalid characters. Only alphanumeric characters, underscores and '@' are allowed.\n");
-                exit(4);
+                if ((u_n[i] < '0' || u_n[i] > '9') &&
+                    u_n[i] != '_' && u_n[i] != '@')
+                {
+                    fprintf(stderr, "Error: Username contains invalid characters.\n");
+                    valid = 0;
+                    break;
+                }
             }
         }
-    }
+    } while (!valid);
     return u_n;
 }
 
 char* change_username(char *user_func)
 {
-    printf("Enter Key... ");
-    int k_i;
+printf("Enter Key... ");
+int k_i;
+do {
     scanf("%d", &k_i);
-    while(getchar() != '\n');
-    if(k_i != Key)
-    {
+    while (getchar() != '\n');
+    if (k_i != Key) {
         fprintf(stderr, "Error: Incorrect key entered.\n");
-        exit(6);
+        printf("Enter Key... ");
     }
+} while (k_i != Key);
+char old_u[50];
+do {
     printf("Enter old username: ");
-    char old_u[50];
     fgets(old_u, sizeof(old_u), stdin);
-    old_u[strcspn(old_u, "\n")] = '\0'; 
-
-    char* nw_u;
-
-    if(strcmp(old_u, user_func) !=0)
-    {
+    old_u[strcspn(old_u, "\n")] = '\0';
+    if (strcmp(old_u, user_func) != 0) {
         fprintf(stderr, "Error: Old username does not match current username.\n");
-        exit(5);
     }
-    nw_u = deb_u();
-    printf("Username changed successfully.\n");
-    return nw_u;
+} while (strcmp(old_u, user_func) != 0);
+char* nw_u = deb_u();
+printf("Username changed successfully.\n");
+return nw_u;
 }
+
 
 int main()
 {
@@ -146,8 +180,8 @@ int main()
    char* def_user = malloc(20 * sizeof(char));
    strcpy(def_user, "admin");
    printf("Hello %s\n", def_user);
-   printf("Your default password is %d\n", def_pass);
-   printf("Your default username is %s\n", def_user);
+   printf("Your default password is '%d'\n", def_pass);
+   printf("YOur default username is '%s'\n", def_user);
    printf("Please change your default credentials\n");
    
    while(1)
